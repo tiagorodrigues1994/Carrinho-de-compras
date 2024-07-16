4a0d253654572e3b3843ecedebcb8dc9c0095665
Tiago Rodrigues <tiago.rodriguesfr2@gmail.com>
2024-07-16 13:46:28 -0300

    carrinho de compras

--- src/pages/Cart/index.tsx
+++ src/pages/Cart/index.tsx
@@ -1,7 +1,28 @@
 export function Cart(){
     return(
-        <div>
-            <h1>Carts</h1>
+        <div className="w-full max-w-7xl px-4 max-auto ">
+            <h1 className="font-bold text-2xl text-center my-4">Meu carrinho</h1>
+
+            <section className="flex items-center justify-between border-b-2 border-gray-300">
+            <img
+                            className="w-28 "
+                            src="https://store.storeimages.cdn-apple.com/4982/as-images.apple.com/is/MV7N2?wid=1144&hei=1144&fmt=jpeg&qlt=95&.v=1551489688005" alt="logo produto" />
+                            <strong>R$1.000</strong>
+                            <div className="flex items-center justify-center gap-3">
+                                <button className="bg-slate-600 rounded text-white font-medium flex items-center justify-center px-2">
+                                    -
+                                </button>
+                                2
+                                <button className="bg-slate-600 rounded text-white font-medium flex items-center justify-center px-2">
+                                    +
+                                </button>
+                            </div>
+
+                            <strong className="float-right">
+                                subTotal: R$1.000
+                            </strong>
+            </section>
+            <p className="font-bold mt-4">Total: R$1.000</p>
         </div>
     )
 }
--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,7 +1,27 @@
+import { BsCartPlus } from "react-icons/bs"
+
+
 export function Home(){
     return(
         <div>
-            <h1>Home</h1>
+           <main className="w-full max-w-7xl px-4 max-auto ">
+                    <h1  className="font-bold text-2xl mb-4 mt-10 text-center">Produtos em alta</h1>
+                    <div className="grid grid-cols-1 gap-6 md:grid-cols-3 lg:grid-cols-5">
+                        <section className="w-full">
+                            <img
+                            className="w-full rounded-lg max-h-70 mb-2"
+                            src="https://store.storeimages.cdn-apple.com/4982/as-images.apple.com/is/MV7N2?wid=1144&hei=1144&fmt=jpeg&qlt=95&.v=1551489688005" />
+                            <p className="font-medium mt-1 mb-2">Airpods Pro</p>
+
+                            <div className="flex gap-3 items-center">
+                                <strong className="text-zin-700/90">
+                                    R$1000
+                                </strong>
+                                <button className="bg-zinc-800 p-1  rounded"><BsCartPlus size={20} color="#fff"/></button>
+                            </div>
+                        </section>
+                    </div>
+           </main>
         </div>
     )
 }
